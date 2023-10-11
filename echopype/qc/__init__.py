c3f515824a3bbb772d7b3f9bc6aee80d99a75954
Caesar Tuguinay <87830138+ctuguinay@users.noreply.github.com>
2023-10-11 16:23:43 -0700

    add orchestrate_reverse_time_check

--- echopype/qc/__init__.py
+++ echopype/qc/__init__.py
@@ -1,3 +1,3 @@
-from .api import coerce_increasing_time, exist_reversed_time
+from .api import coerce_increasing_time, exist_reversed_time, orchestrate_reverse_time_check
 
-__all__ = ["coerce_increasing_time", "exist_reversed_time"]
+__all__ = ["coerce_increasing_time", "exist_reversed_time", "orchestrate_reverse_time_check"]
